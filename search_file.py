67995bfd908bd3773fb11f0ea7fbfbb1080a7c36
matvey102rus <matvejorlov182@gmail.com>
2023-10-11 00:29:30 +0300

    Переделал функцию

--- search_file.py
+++ search_file.py
@@ -1,18 +1,7 @@
-tokenInput = input(("Введите токен: "))
+
 def searchFile(name):
     f = open("file_token/1234.txt")
-    tokenRead = f.readline()
-
-    r = open("file_token/5678.txt")
-    tokenRead2 = r.readline()
-    token = tokenInput
-    if token == tokenRead:
-        token = tokenRead
-
-    elif token == tokenRead2:
-        token = tokenRead2
-    else:
-        token = "Токен не найден!"
-    return token
+    fileRead = f.readline()
+    return fileRead
 
 print(searchFile("12121212"))
