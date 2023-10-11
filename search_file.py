63aa3f0085bc8119a9a6262eefb60f274026af61
matvey102rus <matvejorlov182@gmail.com>
2023-10-11 12:19:45 +0300

    Переделал функцию

--- search_file.py
+++ search_file.py
@@ -1,7 +1,13 @@
-
+fileInput = input()
 def searchFile(name):
-    f = open("file_token/1234.txt")
-    fileRead = f.readline()
+    if fileInput == "1234":
+        f = open("file_token/1234.txt")
+        fileRead = f.readline()
+    elif fileInput == "5678":
+        f = open("file_token/5678.txt")
+        fileRead = f.readline()
+    else:
+        fileRead = "Такой файл не найден!"
     return fileRead
 
 print(searchFile("12121212"))
